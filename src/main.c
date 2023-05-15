207b1cfeb84c32f7d02515ceeb8e913b12107091
Eguelin <eguelin@student.42lyon.fr>
2023-05-15 19:17:33 +0200

    normed

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:30:44 by eguelin           #+#    #+#             */
-/*   Updated: 2023/05/15 19:05:05 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/05/15 19:16:46 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
 	(void)argc;
 	(void)argv;
