0a5d40cf87c9ab7d3fbb05a49785a7b995305abe
Nathan Terrier <naterrie@z1r7p5.42lyon.fr>
2023-05-16 17:42:26 +0200

    Env, echom pwd work, work on cd

---
+++ src/builtin/echo.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naterrie <naterrie@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 13:51:19 by naterrie          #+#    #+#             */
+/*   Updated: 2023/05/16 15:33:48 by naterrie         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	check_option(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str[i] || str[i] != '-' || str[i + 1] != 'n')
+		return (0);
+	i++;
+	while (str[i] && str[i] == 'n')
+		i++;
+	if (str[i] == '\0')
+		return (1);
+	return (0);
+}
+
+static void	ft_writestr(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i + 1])
+	{
+		printf ("%s ", str[i]);
+		i++;
+	}
+	printf ("%s", str[i]);
+}
+
+void	ft_echo(char **cmd)
+{
+	int	i;
+
+	i = 1;
+	if (!cmd[i])
+		return ;
+	if (check_option(cmd[i]))
+	{
+		while (cmd[i] && check_option(cmd[i]))
+			i++;
+		if (cmd[i])
+			ft_writestr(cmd + i);
+	}
+	else
+	{
+		ft_writestr(cmd + i);
+		printf("\n");
+	}
+}
