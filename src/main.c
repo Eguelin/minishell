1ea228512a0f5b41a82950a26245a91d7e02512a
Eguelin <eguelin@student.42lyon.fr>
2023-05-24 19:05:14 +0200

    del main test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:30:44 by eguelin           #+#    #+#             */
-/*   Updated: 2023/05/24 13:42:29 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/05/24 19:02:22 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,6 @@ int	main(int argc, char **argv, char **env)
 	(void)argc;
 	(void)argv;
 	ft_init_minishell(&data, env);
-	while (data.env->next)
-	{
-		printf("%s=%s\n", data.env->name, data.env->content);
-		data.env = data.env->next;
-	}
-	printf("%s=%s\n", data.env->name, data.env->content);
 	ft_env_clear(&data.env);
 	return (0);
 }
