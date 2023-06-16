0e4576cfde192bd2908eb3c40f775fb3041e562f
Eguelin <eguelin@student.42lyon.fr>
2023-06-16 16:20:08 +0200

    update mylib

--- lib/mylib/src/str/ft_strjoin_three.c
+++ lib/mylib/src/str/ft_strjoin_three.c
@@ -1,35 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin_tab.c                                   :+:      :+:    :+:   */
+/*   ft_strjoin_three.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 05:43:08 by eguelin           #+#    #+#             */
-/*   Updated: 2023/06/16 08:49:23 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/06/16 08:51:35 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mylib.h"
 
-char	*ft_strjoin_tab(char **tab)
+char	*ft_strjoin_three(char const *s1, char const *s2, char const *s3)
 {
+	char	*join;
 	size_t	size;
-	int		i;
-	char	*str;
 
-	size = 0;
-	i = 0;
-	if (!tab)
+	if (!s1 || !s2 || !s3)
 		return (NULL);
-	while (tab[i])
-		size += ft_strlen(tab[i++]);
-	str = malloc(sizeof(char) * (size + 1));
-	if (!str)
+	size = ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3) + 1;
+	join = malloc(size);
+	if (!join)
 		return (NULL);
-	i = 0;
-	ft_strlcpy(str, tab[i++], size + 1);
-	while (tab[i])
-		ft_strlcat(str, tab[i++], size + 1);
-	return (str);
+	ft_strlcpy(join, s1, size);
+	ft_strlcat(join, s2, size);
+	ft_strlcat(join, s3, size);
+	return (join);
 }
