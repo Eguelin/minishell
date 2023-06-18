00209ad3d74910a874db261daeb6a5c9514f0bc4
Eguelin <eguelin@student.42lyon.fr>
2023-06-18 19:17:15 +0200

    it's fine

--- src/utils/signal/ft_ctrl_c.c
+++ src/utils/signal/ft_ctrl_c.c
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 13:01:30 by eguelin           #+#    #+#             */
-/*   Updated: 2023/06/17 15:54:00 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/06/18 19:12:25 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	ft_ctrl_c(int signum)
 {
-	if (signum == 2)
-	{
-		g_error = 130;
-		ft_putstr_fd("\n", 1);
-		ft_putstr_fd(ft_prompt(ft_get_data(NULL)), 1);
-		rl_replace_line("", 0);
-	}
+	(void)signum;
+	g_error = 130;
+	rl_on_new_line();
+	ft_putstr_fd("\n", 1);
+	ft_putstr_fd("\001\x1B[1;37m\002(\001\x1B[1;31m\002130\001\x1B[1;37m\002)", \
+	1);
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
